d2bf317871211262636d80f2a89afdba41b0da33
dohe <wahlholz@gmx.de>
2021-12-14 09:22:04 +0100

    Get all data correctly and store it

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
 import time
+from dataclasses import dataclass
+from typing import Dict, Tuple
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -7,6 +9,14 @@ from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
+
+
+@dataclass
+class Book:
+    title: str
+    asin: str
+    progress: int
+    authors: str
 
 
 class Syncer():
@@ -27,13 +37,12 @@ class Syncer():
         )
         self.username = username
         self.password = password
+        self.collected: Dict[str, Book] = {}
 
     def sync(self) -> None:
         self.driver.get("https://read.amazon.com")
         self.login()
-        time.sleep(2)
         self.get_books()
-        # time.sleep(2000)
 
     def stop(self):
         self.driver.close()
@@ -48,24 +57,44 @@ class Syncer():
             email_field.send_keys(self.username)
             password_field.send_keys(self.password)
             submit_button.click()
-            time.sleep(10)
 
     def get_books(self) -> None:
-        books = self.driver.find_elements(By.CSS_SELECTOR, "[data-asin]")
+        books = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_all_elements_located(
+                (By.CSS_SELECTOR, "[data-asin]")
+            )
+        )
         print("Found", len(books), "books")
-        for i in range(len(books)):
-            WebDriverWait(self.driver, 10).until(
-                EC.presence_of_element_located(
+        book_index = 0
+        prev_last_asin = ""
+        while True:
+            books = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_all_elements_located(
                     (By.CSS_SELECTOR, "[data-asin]")
                 )
             )
-            book = self.driver.find_elements(By.CSS_SELECTOR, "[data-asin]")
-            asin = book.get_attribute("data-asin")
-            print(asin)
-            progress = self.get_book(book)
-            print(progress)
+            last_asin = books[-1].get_attribute("data-asin")
 
-    def get_book(self, book: WebElement) -> int:
+            book = books[book_index]
+            asin, title, authors = self.get_book_attributes(book)
+            progress = self.get_progress(book)
+            self.collected[asin] = Book(title, asin, progress, authors)
+            self.store_collected()
+
+            book_index += 1
+            if book_index == len(books):
+                if last_asin == prev_last_asin:
+                    break
+                book_index -= 1
+            prev_last_asin = last_asin
+
+    def get_book_attributes(self, book: WebElement) -> Tuple[str, str, str]:
+        asin = book.get_attribute("data-asin")
+        title = self.driver.find_element(By.ID, "title-" + asin)
+        authors = self.driver.find_element(By.ID, "author-" + asin)
+        return asin, title.text, authors.text
+
+    def get_progress(self, book: WebElement) -> int:
         book.click()
 
         iframe = WebDriverWait(self.driver, 10).until(
@@ -77,15 +106,27 @@ class Syncer():
             EC.presence_of_element_located(
                 (By.ID, "kindleReader_footer_message"))
         )
+        self.wait_for_text(footer)
         progress = footer.text.split("%")[0]
         try:
             progress = int(progress)
         except ValueError:
             pass
-
         self.driver.switch_to.default_content()
-        self.driver.back()  # alternative, click kindleReader_button_close
+        time.sleep(1)
+        self.driver.back()
         return progress
+
+    def store_collected(self):
+        print(self.collected)
+
+    def wait_for_text(self, elem):
+        i = 0
+        while not elem.text:
+            time.sleep(0.5)
+            i += 1
+            if i > 20:
+                break
 
 
 syncer = Syncer()
